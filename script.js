f5734d27697b5c633d78cbe4de256ad8e9976012
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-05-04 21:24:46 -0700

    Gets grid dimension input via form and submit

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 //First I want my grid to be created based on the value of gridDimension
-let gridInput = prompt('Enter dimensions: ', 0);
-let gridSquares = gridInput * gridInput;
+const myForm = document.querySelector('#myForm');
+const gridDim = document.querySelector('#gridDim');
 const container = document.querySelector('.container');
-for(i = 0; i < gridSquares; i++){
-    let newDiv = document.createElement('div');
-    container.appendChild(newDiv);
+const numInputs = document.querySelectorAll('input[type=number]');
+
+numInputs.forEach(function(input){
+    input.addEventListener('change', function(e){
+        if(e.target.value == ''){
+            e.target.value = 1;
+        }
+    })
+});
+function createSquares(){
+    let gridSquares = gridDim.value * gridDim.value;
+    for(i = 0; i < gridSquares; i++){
+        let newDiv = document.createElement('div');
+        container.appendChild(newDiv);
+    }
 }
-container.setAttribute('style', `grid-template: repeat(${gridInput}, 1fr) / repeat(${gridInput}, 1fr);`);
+function setDimensions(){
+    container.setAttribute('style', `grid-template: repeat(${gridDim.value}, 1fr) / repeat(${gridDim.value}, 1fr);`);
+}
+myForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    console.log(`${gridDim.value}` + ' x ' + `${gridDim.value}`);
+    createSquares();
+    setDimensions();
+});
+createSquares();
+setDimensions();
